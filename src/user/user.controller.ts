a3313d3126c16f560693f939f6caef69353eb62b
fatih.durna <fdfatihdurna@gmail.com>
2023-09-04 23:10:42 +0300

    useragent dev

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,19 +6,33 @@ import {
   Param,
   Post,
   Put,
+  Req,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserCreateDto, UserUpdateDto } from 'tools/dtos/user.dto';
 import { UserModel } from 'tools/models/user.model';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Roles } from 'libs/decorators/role.decorators';
+import * as useragent from 'express-useragent';
 
 @ApiTags('User')
 @ApiBearerAuth()
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+  @Get('browser-info')
+  getBrowserInfo(@Req() request) {
+    const userAgent = request.headers['user-agent'];
+    const userAgentInfo = useragent.parse(userAgent);
 
+    const browserInfo = {
+      browser: userAgentInfo.browser,
+      version: userAgentInfo.version,
+      platform: userAgentInfo.platform,
+    };
+
+    return browserInfo;
+  }
   @Post()
   @Roles('Developer')
   async createUser(@Body() body: UserCreateDto): Promise<UserModel> {
